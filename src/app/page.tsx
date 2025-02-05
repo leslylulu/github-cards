dca8b2c94251006ab6e17f13526f32694739b1e9
zhan0858 <zhan0858@algonquinlive.com>
2025-02-05 11:30:33 -0500

    responsive UI and basic framework

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,55 +1,68 @@
+"use client";
+import { useState } from "react";
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [username, setUsername] = useState("");
+  const [userData, setUserData] = useState(null);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const toggleSidebar = () => {
+    setIsSidebarOpen((prev) => !prev);
+  };
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("Submitted username:", username);
+    // You can now use the username value as needed
+    // try {
+    //   const response = await fetch(`https://api.github.com/users/${username}`);
+    //   if (!response.ok) {
+    //     throw new Error("Network response was not ok");
+    //   }
+    //   const data = await response.json();
+    //   console.log("GitHub user data:", data);
+    //   setUserData(data);
+    // } catch (error) {
+    //   console.error("There was a problem with the fetch operation:", error);
+    // }
+  };
+
+  return (
+    <div className="min-h-screen flex flex-col p-2 pb-8 gap-3 sm:p-3 font-[family-name:var(--font-geist-sans)]">
+      <div className={`${isSidebarOpen ? "hidden" : "flex"} block lg:hidden`}>
+        <button
+          onClick={toggleSidebar}
+        >
+          ☰
+        </button>
+      </div>
+
+      <main className="w-full h-full flex-grow lg:grid lg:grid-cols-[1fr_3fr] gap-4 p-6">
+        <div className={`${isSidebarOpen ? "flex" : "hidden"} lg:flex lg:flex-col lg:w-full `}>
+          <div className="font-bold text-center">Github Cards</div>
+          <form onSubmit={handleSubmit}>
+            <div>
+              <label htmlFor="username" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Github username</label>
+              <input
+                value={username}
+                onChange={(event) => setUsername(event.target.value)}
+                type="text"
+                id="username"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Input Github Usernam" required />
+            </div>
+            <button type="submit" className="mt-3 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+          </form>
+
+
+        </div>
+        <div className="grid grid-cols-3">
+          <div className="col-span-2 bg-gray-800/10 p-3 flex justify-center items-center">I am a card</div>
+          <div className="col-span-1 bg-purple-500/25 p-3">I am a right side</div>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+
+      <footer className="flex gap-6 flex-wrap items-center justify-center">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
           href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
@@ -63,7 +76,7 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Learn
+          Github Docs
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
@@ -78,7 +91,7 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Examples
+          My website
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
@@ -93,8 +106,9 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Go to nextjs.org →
+          Explore more nextjs.org →
         </a>
+        <p>© 2025 Lulu - All rights reserved.</p>
       </footer>
     </div>
   );
